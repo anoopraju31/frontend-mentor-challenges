d3b8ff079365abba94551a520313c57954aeb872
anoopraju31 <anoopraju31@gmail.com>
2023-06-15 11:57:23 +0530

    QR Code Component : qr code component created

--- qr-code-component/src/App.js
+++ qr-code-component/src/App.js
@@ -1,7 +1,9 @@
+import { QRCode } from './components'
+
 const App = () => {
 	return (
 		<div className='w-full min-h-screen bg-light-gray flex justify-center items-center'>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			<QRCode />
 		</div>
 	)
 }
