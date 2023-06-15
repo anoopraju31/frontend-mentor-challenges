cfc441955b8fc3a337d85c1c3dd8639e24a8c99a
anoopraju31 <anoopraju31@gmail.com>
2023-06-15 11:53:04 +0530

    QR Code Component : app component style added

--- qr-code-component/src/App.js
+++ qr-code-component/src/App.js
@@ -1,6 +1,6 @@
 const App = () => {
 	return (
-		<div className=''>
+		<div className='w-full min-h-screen bg-light-gray flex justify-center items-center'>
 			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
 		</div>
 	)
